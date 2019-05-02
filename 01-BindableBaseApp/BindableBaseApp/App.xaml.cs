a8f7975ad3ea5d34bf5f73793f1590fa10ede6a6
Frepe <needvitneedfgt@gmail.com>
2019-05-02 11:50:55 +0900

    Create a project that demonstrates the BindableBase class
    Initial Commit

---
+++ 01-BindableBaseApp/BindableBaseApp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BindableBaseApp.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace BindableBaseApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
