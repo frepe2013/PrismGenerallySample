234e288a8c00e976f2b638851b55777b88132fcb
Frepe <needvitneedfgt@gmail.com>
2019-05-10 14:36:51 +0900

    Create a project that demonstrates the DelegateCommand and InvokeCommandAction class

---
+++ 03-DelegateCommandApp/DelegateCommandApp/ViewModels/BookVm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace DelegateCommandApp.ViewModels
+{
+    public class BookVm
+    {
+        public string Title { get; }
+
+        public string Author { get; }
+
+        public BookVm(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ 03-DelegateCommandApp/DelegateCommandApp/ViewModels/ListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DelegateCommandApp.ViewModels
+{
+    public class ListViewModel : BindableBase
+    {
+        private ObservableCollection<BookVm> _books;
+
+        public ObservableCollection<BookVm> Books
+        {
+            get => _books;
+            set => SetProperty(ref _books, value);
+        }
+
+        public ListViewModel()
+        {
+            Books = new ObservableCollection<BookVm>(new List<BookVm>
+            {
+                new BookVm("Test-Driven Development", "Kent Beck"),
+                new BookVm("The Healthy Programmer", "Joe Kutner"),
+                new BookVm("Effective C#", "Bill Wagner"),
+            });
+        }
+    }
+}
