8f11d634d142bc17c639c6b004e698bc91c1048e
Frepe <needvitneedfgt@gmail.com>
2019-05-14 11:59:30 +0900

    Create a project that demonstrates the ErrorsContainer class
    04-Project Copy

---
+++ 05-BasicApp/BasicApp/ViewModels/BookVm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicApp.ViewModels
+{
+    public class BookVm
+    {
+        public string Title { get; }
+
+        public string Author { get; }
+
+        public BookVm(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ 05-BasicApp/BasicApp/ViewModels/ListViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using BasicApp.Entities;
+
+namespace BasicApp.ViewModels
+{
+    public class ListViewModel : BindableBase
+    {
+        private IRegionManager _regionManager;
+
+        private ObservableCollection<BookVm> _books;
+
+        public ObservableCollection<BookVm> Books
+        {
+            get => _books;
+            set => SetProperty(ref _books, value);
+        }
+
+        public DelegateCommand<BookVm> BookSelectedCommand { get; set; }
+
+        public ListViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            Books = new ObservableCollection<BookVm>();
+            using (var context = new ShelfContext())
+            {
+                var bookList = context.Books.ToList();
+                var vms = bookList.Select(book => new BookVm(book.Title, book.Author));
+                Books.AddRange(vms);
+            }
+
+            BookSelectedCommand = new DelegateCommand<BookVm>(ExecuteBookSelectedCommand);
+        }
+
+        private void ExecuteBookSelectedCommand(BookVm data)
+        {
+            if (data == null) return;
+
+            var parameters = new NavigationParameters { { "book", data } };
+            _regionManager.RequestNavigate("RightRegion", "Detail", parameters);
+        }
+    }
+}
