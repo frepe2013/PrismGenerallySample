192a0c5bf70f494fb521e8c32eb8726825fe3910
Frepe <needvitneedfgt@gmail.com>
2019-05-23 11:18:44 +0900

    Create a project that demonstrates PopupWindowAction class
    07-Project Copy

---
+++ 08-BasicApp.sln/BasicApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using BasicApp.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using BasicApp.Entities;
+
+namespace BasicApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<Detail>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Database.SetInitializer(new DbInitializer());
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ 08-BasicApp.sln/BasicApp/ViewModels/BookVm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using BasicApp.Entities;
+
+namespace BasicApp.ViewModels
+{
+    public class BookVm : BindableBase
+    {
+        private bool _isEdited;
+
+        internal Book Model { get; }
+
+        public int Id => Model.Id;
+
+        public string Title => Model.Title;
+
+        public string Author => Model.Author;
+
+        public string TitleForList
+        {
+            get
+            {
+                if (IsEdited)
+                {
+                    return Title + " *";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
+
+        public bool IsEdited
+        {
+            get => _isEdited;
+            set
+            {
+                if (value == _isEdited)
+                {
+                    return;
+                }
+
+                _isEdited = value;
+
+                RaisePropertyChanged(nameof(TitleForList));
+            }
+        }
+
+        public BookVm(Book book)
+        {
+            Model = book;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
